3200e20368bfc7815d52170c28b99b10d461f525
David <davidponceg207@gmail.com>
2025-02-06 08:28:29 -0600

    Calculator Button

--- src/presentation/screens/CalculatorScreen.tsx
+++ src/presentation/screens/CalculatorScreen.tsx
@@ -1,5 +1,6 @@
 import { Pressable, Text, View } from "react-native"
-import { styles } from "../../config/theme/app-theme"
+import { colors, styles } from "../../config/theme/app-theme"
+import { CalculatorButton } from "../components/CalculatorButton"
 
 export const CalculatorScreen = () => {
   return (
@@ -13,10 +14,42 @@ export const CalculatorScreen = () => {
         </View>
 
         <View style={styles.row}>
-          <Pressable style={styles.button}>
-            <Text style={styles.buttonText}>1</Text>
-          </Pressable>
+
+          <CalculatorButton label="C" color={colors.lightGray} blackText />
+          <CalculatorButton label="+/-" color={colors.lightGray} blackText />
+          <CalculatorButton label="del" color={colors.lightGray} blackText />
+          <CalculatorButton label="÷" color={colors.orange} />
+
+
         </View>
+
+        <View style={styles.row}>
+          <CalculatorButton label="7" />
+          <CalculatorButton label="8" />
+          <CalculatorButton label="9" />
+          <CalculatorButton label="x" color={colors.orange} />
+        </View>
+
+        <View style={styles.row}>
+          <CalculatorButton label="4"/>
+          <CalculatorButton label="5"/>
+          <CalculatorButton label="6"/>
+          <CalculatorButton label="-" color={colors.orange} />
+        </View>
+
+        <View style={styles.row}>
+          <CalculatorButton label="1" />
+          <CalculatorButton label="2" />
+          <CalculatorButton label="3" />
+          <CalculatorButton label="+" color={colors.orange} />
+        </View>
+
+        <View style={styles.row}>
+          <CalculatorButton label="0" doubleSize={true}/>
+          <CalculatorButton label="." />
+          <CalculatorButton label="=" color={colors.orange} />
+        </View>
+
     </View>
   )
 }
