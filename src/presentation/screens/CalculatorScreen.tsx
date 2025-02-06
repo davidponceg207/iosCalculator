c90570cc8d3a7b23136cc44685f097f1358153c8
David <davidponceg207@gmail.com>
2025-02-06 07:57:37 -0600

    Add initial styles for button

--- src/presentation/screens/CalculatorScreen.tsx
+++ src/presentation/screens/CalculatorScreen.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native"
+import { Pressable, Text, View } from "react-native"
 import { styles } from "../../config/theme/app-theme"
 
 export const CalculatorScreen = () => {
@@ -10,6 +10,12 @@ export const CalculatorScreen = () => {
             <Text style={styles.mainResult}>1500</Text>
             <Text style={ styles.subResult }>15</Text>
 
+        </View>
+
+        <View style={styles.row}>
+          <Pressable style={styles.button}>
+            <Text style={styles.buttonText}>1</Text>
+          </Pressable>
         </View>
     </View>
   )
