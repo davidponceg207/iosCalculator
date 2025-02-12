4d6fb84815c1dc0c9bf4a1e37e80eeba1602bd28
David <davidponceg207@gmail.com>
2025-02-12 08:09:26 -0600

    Add onPress function to CalculatorButton

--- src/presentation/components/CalculatorButton.tsx
+++ src/presentation/components/CalculatorButton.tsx
@@ -5,17 +5,21 @@ interface Props {
     label: string,
     color?: string,
     doubleSize?: boolean,
-    blackText?: boolean
+    blackText?: boolean,
+    onPress: () => void,
 }
 
 export const CalculatorButton = ({
     label,
     color = colors.darkGray,
     doubleSize = false,
-    blackText = false
+    blackText = false,
+    onPress
 }: Props) => {
     return (
-        <Pressable style={ ({pressed}) => ({
+        <Pressable
+        onPress={ () => onPress()}
+        style={ ({pressed}) => ({
             ...styles.button,
             backgroundColor: color,
             opacity: (pressed) ? 0.8 : 1,
